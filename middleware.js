c3ea4eab650e8c34c0a38474411afabebd755152
Manoj <madhushreegm@MacBook-Air.local>
2025-07-26 17:23:11 +0530

    Migrated auth to clerk

--- middleware.js
+++ middleware.js
@@ -6,5 +6,5 @@ import { authMiddleware } from "@clerk/nextjs/server";
 export default authMiddleware({});
  
 export const config = {
-  matcher: ['/((?!.+\.[\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.+\.[\w]+$|_next|sign-in|sign-up|api|trpc).*)', '/'],
 };
